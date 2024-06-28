f5419e655a3eb861f4b4bdf8b0706be08167ed20
Swathi Reddy <swathireddyg54@gmail.com>
2024-06-28 11:49:57 +0530

    removes ownable.sol import

--- src/Gas.sol
+++ src/Gas.sol
@@ -1,15 +1,13 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.26;
 
-import "./Ownable.sol";
-
 contract Constants {
     uint256 public tradeFlag = 1;
     uint256 public basicFlag = 0;
     uint256 public dividendFlag = 1;
 }
 
-contract GasContract is Ownable, Constants {
+contract GasContract is Constants {
     uint256 public totalSupply = 0; // cannot be updated
     uint256 public paymentCounter = 0;
     mapping(address => uint256) public balances;
